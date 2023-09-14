b6fc8b0ed90f8c646207a242d6f94ab90f9c7c33
KauaCaze-Fatec <143725974+KauaCaze-Fatec@users.noreply.github.com>
2023-09-14 11:56:51 -0300

    Começo da codificação das paginas de questionario

---
+++ src/js/questionario.js
@@ -0,0 +1,11 @@
+let gaba = [A,B,C];
+
+let res = [A,C,B];
+
+function coparar() {
+    if(gaba == res){
+        console.log = "Tudo certo"
+    } else {
+        console.log = "Tudo errado"
+    }
+}
