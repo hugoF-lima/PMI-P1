adf9b39320b282c7715eba588fb2b3d199601305
KauaCaze-Fatec <143725974+KauaCaze-Fatec@users.noreply.github.com>
2023-09-15 16:56:23 -0300

    Criação e leve estilisação da TopNav, mudança no método js

--- src/js/questionario.js
+++ src/js/questionario.js
@@ -1,11 +1,200 @@
-let gaba = [A,B,C];
-
-let res = [A,C,B];
-
-function coparar() {
-    if(gaba == res){
-        console.log = "Tudo certo"
-    } else {
-        console.log = "Tudo errado"
+const gaba = ()=>{
+    return{
+            "q1": {
+                "img":"",
+                "question":"A partir das informações apresentadas, é correto afirmar que",
+                "options": [
+                    "o fator ambiental e o fator demográfico afetam a mobilidade social observada, sendo ela menor nos países que apresentam as maiores taxas de natalidade.",
+        
+                    "a baixa organização social dos economicamente menos favorecidos determina a baixa mobilidade social da base para o topo da pirâmide.",
+        
+                    "a mobilidade social é caracterizada por um fator ancestral que se revela ao longo das gerações, sendo um limitador da efi cácia de políti cas públicas de redução das desigualdades sociais.",
+        
+                    "a análise de mobilidade social permite a observação de um ciclo vicioso, que se caracteriza por uma subida nas camadas sociais seguida de uma queda, repetindo-se esse ciclo de modo sucessivo.",
+        
+                    "a ascensão social depende de fatores viabilizadores que estão fora do alcance das camadas pobres, o que ocasiona confl itos sociais em busca do acesso a tais fatores."
+                ],
+                "answer":"a ascensão social depende de fatores viabilizadores que estão fora do alcance das camadas pobres, o que ocasiona confl itos sociais em busca do acesso a tais fatores."
+            },
+            "q2":{
+                "img1":"",
+                "img2":"",
+                "texto":" Considerando as informações apresentadas nos textos, avalie as asserções a seguir e a relação proposta entre elas.",
+                "img3":"",
+                "question":"A respeito dessas asserções, assinale a opção correta.",
+                "options":[
+                    "As asserções I e II são proposições verdadeiras, e a II é uma justi fi cati va correta da I.",
+                    "As asserções I e II são proposições verdadeiras, mas a II não é uma justi fi cati va correta da I.",
+                    "A asserção I é uma proposição verdadeira, e a II é uma proposição falsa.",
+                    "A asserção I é uma proposição falsa, e a II é uma proposição verdadeira.",
+                    "As asserções I e II são proposições falsas."
+                ],
+                "answer":"A asserção I é uma proposição verdadeira, e a II é uma proposição falsa."
+                
+            },
+            "q3":{
+                "img1":"",
+                "img2":"",
+                "question":"É correto o que se afirma em",
+                "options":[
+                    "I, apenas.",
+                    "II, apenas.",
+                    "I e III, apenas.",
+                    "II e III, apenas.",
+                    "I, II e III."
+                ],
+                "answer":"II, apenas."
+            },
+            "q4":{
+                "img":"",
+                "question":"A respeito dessas asserções, assinale a opção correta.",
+                "options":[
+                    "As asserções I e II são proposições verdadeiras, e a II é uma justificativa correta da I.",
+                    "As asserções I e II são proposições verdadeiras, mas a II não é uma justificativa correta da I.",
+                    "A asserção I é uma proposição verdadeira, e a II é uma proposição falsa.",
+                    "A asserção I é uma proposição falsa, e a II é uma proposição verdadeira.",
+                    "As asserções I e II são proposições falsas."
+                ],
+                "answer":"As asserções I e II são proposições verdadeiras, mas a II não é uma justificativa correta da I."
+            },
+            "q5":{
+                "img1":"",
+                "img2":"",
+                "img3":"",
+                "question":"É correto apenas o que se afirma em",
+                "options":[
+                    "I.",
+                    "II.",
+                    "I e III.",
+                    "II e IV.",
+                    "III e IV."
+                ],
+                "answer":"I."
+            },
+            "q6":{
+                "img1":"",
+                "question":"A respeito dessas asserções, assinale a opção correta.",
+                "options":[
+                    "As asserções I e II são proposições verdadeiras, e a II é uma justificativa correta da I.",
+                    "As asserções I e II são proposições verdadeiras, mas a II não é uma justificativa correta da I.",
+                    "A asserção I é uma proposição verdadeira, e a II é uma proposição falsa.",
+                    "A asserção I é uma proposição falsa, e a II é uma proposição verdadeira.",
+                    "As asserções I e II são proposições falsas."
+                ],
+                "answer":"As asserções I e II são proposições verdadeiras, e a II é uma justificativa correta da I."
+            },
+            "q7":{
+                "img1":"",
+                "img2":"",
+                "question":"É correto o que se afirma em",
+                "options":[
+                    "I, apenas.",
+                    "III, apenas.",
+                    "I e II, apenas.",
+                    "II e III, apenas.",
+                    "I, II e III."
+                ],
+                "answer":"I e II, apenas."
+            },
+            "q8":{
+                "img1":"",
+                "question":"É correto apenas o que se afirma em",
+                "options":[
+                    "I e II.",
+                    "I e IV.",
+                    "II e III.",
+                    "I, III e IV.",
+                    "II, III e IV."
+                ],
+                "answer":"I, III e IV."
+            },
+            "q9":{
+                "img":"",
+                "question":"Considerando que em ambientes diferentes são necessários algoritmos diferentes de escalonamento, garantindo assim que seja maximizado o uso de seus recursos, assinale a opção que apresenta um algoritmo de escalonamento seguido do tipo de ambiente no qual deva ser implementado",
+                "options":[
+                    " Primeiro a chegar, último a sair (first in, last out - FILO); propício para sistemas de tempo real.",
+                    " Escalonamento por taxas monotônicas (rate monotonic scheduling - RMS); propício para sistemas em lote.",
+                    "Tarefa mais curta primeiro; propício para sistemas interativos.",
+                    "Escalonamento por chave circular (round-robin); propício para sistemas de tempo real.",
+                    "Escalonamento por prioridades; propício para sistemas interativos."
+                ],
+                "answer":"Escalonamento por prioridades; propício para sistemas interativos."
+            },
+            "q10":{
+                "img":"",
+                "question":"Nesse contexto, assinale a alternativa que representa, respectivamente, as estruturas de dados implementadas pelas classes A, B, C e D.",
+                "options":[
+                    "Lista circular, lista simplesmente ligada, pilha e fila.",
+                    "Deque, lista simplesmente ligada, pilha e fila.",
+                    "Lista duplamente ligada, lista simplesmente ligada, fila e pilha.",
+                    "Pilha, fila, deque e lista simplesmente encadeada.",
+                    "Deque, pilha, lista ligada e fila."
+                ],
+                "answer":"Deque, lista simplesmente ligada, pilha e fila."
+            },
+            "q11":{
+                "img1":"",
+                "img2":"",
+                "img3":"",
+                "question":"É correto apenas o que se afirma em",
+                "options":[
+                    "I e IV.",
+                    "I e V.",
+                    "II e III.",
+                    "II e IV.",
+                    "III e V."
+                ],
+                "answer":"II e IV."
+            },
+            "q12":{
+                "img":"",
+                "question":"É correto apenas o que se afirma em",
+                "options":[
+                    "I e II.",
+                    "I e III.",
+                    "II e IV.",
+                    "I, III e IV.",
+                    "II, III e IV."
+                ],
+                "answer":"I e III."
+            },
+            "q13":{
+                "img":"",
+                "question":"Considerando o texto apresentado, assinale a opção correta sobre o desenvolvimento iterativo e evolutivo.",
+                "options":[
+                    "A mudança nos requisitos do sistema é algo que gera atraso no desenvolvimento, por isso é aconselhável evitá-la",
+                    "O ciclo de desenvolvimento possui duração fixa, porém, durante o desenvolvimento, poderá ser alterado no caso de sistemas críticos.",
+                    "O teste de usabilidade deve ser realizado no último ciclo, pois será o momento em que o usuário consegue testar todas as funcionalidades",
+                    "O subsistema gerado pela implementação dos requisitos no fim de uma iteração poderá ser utilizado pelo cliente como protótipo.",
+                    "O documento de teste de usabilidade deve contemplar os critérios de acessibilidade para atender a todos os usuários do sistema."
+                ],
+                "answer":"O documento de teste de usabilidade deve contemplar os critérios de acessibilidade para atender a todos os usuários do sistema."
+                
+            },
+            "q14":{
+                "img1":"",
+                "img2":"",
+                "question":"A respeito dessas asserções, assinale a opção correta.",
+                "options":[
+                    "As asserções I e II são proposições verdadeiras, e a II é uma justificativa correta da I.",
+                    "As asserções I e II são proposições verdadeiras, mas a II não é uma justificativa correta da I.",
+                    "A asserção I é uma proposição verdadeira, e a II é uma proposição falsa.",
+                    "A asserção I é uma proposição falsa, e a II é uma proposição verdadeira.",
+                    "As asserções I e II são proposições falsas."
+                ],
+                "answer":"A asserção I é uma proposição verdadeira, e a II é uma proposição falsa."
+            },
+            "q15":{
+                "img":"",
+                "question":"É correto o que se afirma em",
+                "options":[
+                    "II, apenas.",
+                    "III, apenas.",
+                    "I e II, apenas.",
+                    "I e III, apenas.",
+                    "I, II e III."
+                ],
+                "answer":"I e II, apenas."
+        }
     }
-}
+};
