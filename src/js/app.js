b56c8fdafa1482384ba7cc17b76b182fbfe38a40
KauaCaze-Fatec <143725974+KauaCaze-Fatec@users.noreply.github.com>
2023-09-22 08:27:57 -0300

    Concerto da função de troca de conteudo innerHTML

--- src/js/app.js
+++ src/js/app.js
@@ -14,16 +14,9 @@ ante.addEventListener('click', ()=>{
 
 function mostrarQuest(indice){
     const questao = gaba[indice];
+    console.log(questao.enunciado);
 
-    content.innerHTML += `
-    <header>
-        <ul class="topNav">
-            <li class="dif"><a href=""><img src="" alt="Logo ENADE"></a></li>
-            <li><a href="">Tutorial</a></li>
-            <li><a href="">Sobre</a></li>
-        </ul>
-    </header>
-
+    content.innerHTML = `
         <article class="container">
             <h1>Exame de Ciência da Computação (Bacharelado)</h1> 
             <h3>Questão ${questao.id}</h3>
@@ -47,20 +40,7 @@ function mostrarQuest(indice){
                     })
                     .join('')}
             </div>
-            
-            <div class="btns">
-                <button type="submit" class="dif" id="voltar">Anterior</button>
-                <button type="submit" id="verif">Verificar</button>
-                <button type="submit" id="prox">Próxima</button>
-            </div>
-
         </article>
-  
-    <footer class="botomNav">
-        <img src="../../img/logoFatec.png" alt="Logo FATEC">
-        <img src="../../img/logoADS.png" alt="Logo ADS">
-        <img src="../../img/logoCPS.png" alt="Logo CPS">
-    </footer>
     `;
 }
 
@@ -78,6 +58,6 @@ function altQuest(direcao){
             indiceAtual = 37;
         }
     }
-
+    console.log(indiceAtual)
     mostrarQuest(indiceAtual);
 }
--- src/js/gaba.js
+++ src/js/gaba.js
@@ -7,7 +7,7 @@ export const gaba = [
             img:true,
             imgScr:'../img/Q1_text.PNG',
                 img: true,
-                imgScr: '../../img/Q1_text.PNG'
+                imgScr: '../img/Q1_text.PNG'
             },
         ],
         opcoes: [
@@ -67,39 +67,6 @@ export const gaba = [
             }
         ],
         opcoeCorreta:'C'
-    },
-    {
-        id: '02',
-        enunciado: 'A partir das informações apresentadas, é correto afirmar que:',
-        img: [
-            {
-                img: true,
-                imgScr: '../../img/Q1_text.PNG'
-            },
-        ],
-        opcoes: [
-            {
-                id: 'A',
-                texto: 'o fator ambiental e o fator demográfico afetam a mobilidade social observada, sendo ela menor nos países que apresentam as maiores taxas de natalidade.'
-            },
-            {
-                id: 'B',
-                texto: 'a baixa organização social dos economicamente menos favorecidos determina a baixa mobilidade social da base para o topo da pirâmide.'
-            },
-            {
-                id: 'C',
-                texto: 'a mobilidade social é caracterizada por um fator ancestral que se revela ao longo das gerações, sendo um limitador da efi cácia de políti cas públicas de redução das desigualdades sociais.'
-            },
-            {
-                id: 'D',
-                texto: 'a análise de mobilidade social permite a observação de um ciclo vicioso, que se caracteriza por uma subida nas camadas sociais seguida de uma queda, repetindo-se esse ciclo de modo sucessivo.'
-            },
-            {
-                id: 'E',
-                texto: 'a ascensão social depende de fatores viabilizadores que estão fora do alcance das camadas pobres, o que ocasiona confl itos sociais em busca do acesso a tais fatores.'
-            },
-        ],
-        opcaoCorreta: 'E',
     },
     {
         id: '03',
@@ -1229,7 +1196,7 @@ export const gaba = [
         img: [
             {
                 img: true,
-                imgScr: '../../img/Q1_text.PNG'
+                imgScr: '../img/Q1_text.PNG'
             },
         ],
         opcoes: [
